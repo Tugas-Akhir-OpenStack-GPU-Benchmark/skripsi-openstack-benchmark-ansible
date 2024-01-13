a0f894a971e1ea7ac89f44898e919f7deefd4abb
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-14 01:33:19 +0700

    fix: NAMD ubuntu fixed

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# run with sudo
 
-ansible-galaxy install nvidia.nvidia_driver
-ansible-playbook  main.yaml -i inventory.txt
+ansible-galaxy install nvidia.nvidia_driver,2.3.0
+sudo ansible-playbook  main.yaml -i inventory.txt
