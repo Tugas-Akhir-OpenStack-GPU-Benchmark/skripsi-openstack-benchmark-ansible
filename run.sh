1ea5d5b48e6cd11334d5be9935a8c08b2ce461b6
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-21 03:07:16 +0700

    fix: now we fixed pytorch not detecting Nvidia GPU

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ ansible-galaxy install nvidia.nvidia_driver,2.3.0
 #ansible-galaxy role install jedimt.cuda,1.0.2
 
 
-#sudo ansible-playbook  main.yaml -i inventory.txt
+sudo ansible-playbook  main.yaml -i inventory.txt
 #sudo ansible-playbook  main.yaml -i inventory.txt --start-at-task="chmod prime-run"
-sudo ansible-playbook  main.yaml -i inventory.txt --start-at-task="install libfreeimage"
+#sudo ansible-playbook  main.yaml -i inventory.txt --start-at-task="install libfreeimage"
 #
