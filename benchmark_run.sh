8e7db13d0315d3aaee5355143a6d4796f623b49a
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-23 01:15:23 +0700

    refactor: move inventory and main.yaml to more specific folder

---
+++ benchmark_run.sh
@@ -0,0 +1,7 @@
+# run with sudo
+
+ansible-galaxy install nvidia.nvidia_driver,2.3.0
+
+sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=$1"
+#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="chmod prime-run" -e "ansible_host=$1"
+#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="install libfreeimage" -e "ansible_host=$1"
--- run.sh
+++
@@ -1,9 +0,0 @@
-# run with sudo
-
-ansible-galaxy install nvidia.nvidia_driver,2.3.0
-#ansible-galaxy role install jedimt.cuda,1.0.2
-
-
-sudo ansible-playbook  main.yaml -i inventory.txt -e "ansible_host=$1"
-#sudo ansible-playbook  main.yaml -i inventory.txt --start-at-task="chmod prime-run" -e "ansible_host=$1"
-#sudo ansible-playbook  main.yaml -i inventory.txt --start-at-task="install libfreeimage" -e "ansible_host=$1"
