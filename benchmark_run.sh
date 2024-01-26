81773cb79fb44df44b6fb564be245e73643d054e
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-26 17:54:03 +0700

    edit benchmark_run.sh

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -4,6 +4,7 @@
 
 ansible-galaxy install nvidia.nvidia_driver,2.3.0
 
-sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=$1"
+sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=10.119.106.22 ansible_password=k0s0ng2024"
+
+#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=$1"
 #sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="chmod prime-run" -e "ansible_host=$1"
-#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="install libfreeimage" -e "ansible_host=$1"
