271908173c7e91dcafc932acbfadc32fb067869f
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-26 18:11:33 +0700

    add export ANSIBLE_HOST_KEY_CHECKING=False to skip fingerprint checking

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -4,7 +4,9 @@
 
 ansible-galaxy install nvidia.nvidia_driver,v2.3.0
 
-sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=10.119.106.22 ansible_password=k0s0ng2024"
+
+sudo env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt \
+  -e "ansible_host=10.119.106.22 ansible_password=k0s0ng2024"
 
 #sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=$1"
 #sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="chmod prime-run" -e "ansible_host=$1"
