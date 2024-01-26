ed1a938166dd0ca98b49e3d49d663351236ff156
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-26 18:18:40 +0700

    refactor benchmark_run.sh and move some variables from benchmark/inventory.txt to benchmark_run.sh

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -4,9 +4,14 @@
 
 ansible-galaxy install nvidia.nvidia_driver,v2.3.0
 
+MAIN_SCRIPT="./tasks/benchmark/main.yaml"
+INVENTORY="./tasks/benchmark/inventory.txt"
 
-sudo env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt \
-  -e "ansible_host=10.119.106.22 ansible_password=k0s0ng2024"
+SERVER_FASILKOM_EXTRAVARS="ansible_host=10.119.106.22 ansible_password=k0s0ng2024"
 
-#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "ansible_host=$1"
-#sudo ansible-playbook  ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt --start-at-task="chmod prime-run" -e "ansible_host=$1"
+sudo env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook "$MAIN_SCRIPT" -i "$INVENTORY" -e "$SERVER_FASILKOM_EXTRAVARS"
+
+
+export ANSIBLE_EXTRAVARS = "ansible_host=$1 ansible_user=immanuel01 ansible_ssh_private_key_file=~/.ssh/gcp"
+#sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "$ANSIBLE_EXTRAVARS"
+#sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "$ANSIBLE_EXTRAVARS" --start-at-task="chmod prime-run"
