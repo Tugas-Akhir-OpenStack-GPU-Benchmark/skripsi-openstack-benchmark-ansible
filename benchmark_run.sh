e51e77505689fe91c977b0a63f70d731a25eaf3d
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-01-27 01:46:32 +0700

    feat: add various glmark resolution size

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -12,6 +12,6 @@ SERVER_FASILKOM_EXTRAVARS="ansible_host=10.119.106.22 ansible_password=k0s0ng202
 sudo env ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook "$MAIN_SCRIPT" -i "$INVENTORY" -e "$SERVER_FASILKOM_EXTRAVARS"
 
 
-export ANSIBLE_EXTRAVARS = "ansible_host=$1 ansible_user=immanuel01 ansible_ssh_private_key_file=~/.ssh/gcp"
+export ANSIBLE_EXTRAVARS="ansible_host=$1 ansible_user=immanuel01 ansible_ssh_private_key_file=~/.ssh/gcp"
 #sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "$ANSIBLE_EXTRAVARS"
 #sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "$ANSIBLE_EXTRAVARS" --start-at-task="chmod prime-run"
