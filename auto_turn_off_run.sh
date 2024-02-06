a1340688255475e8665e2e40b148a7f235d6e972
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-06 19:52:34 +0700

    refactor: move auto_turnoff.yaml to tasks/auto_turnoff/

--- auto_turn_off_run.sh
+++ auto_turn_off_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # need trailing comma
-sudo ansible-playbook auto_turnoff.yaml -i auto_turnoff_inventory.txt -e "ansible_host=$1"
+sudo ansible-playbook ./tasks/auto_turnoff/auto_turnoff.yaml -i ./tasks/auto_turnoff/auto_turnoff_inventory.txt -e "ansible_host=$1"
---
+++ gcp-spec/gcp-jakarta-2-nic.sh
@@ -0,0 +1,37 @@
+# GPU
+gcloud compute instances create gpu-jakarta-1 \
+    --project=law-2006463162 \
+    --zone=asia-southeast2-b \
+    --machine-type=custom-4-8192 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --network-interface=aliases=/26,network-tier=STANDARD,stack-type=IPV4_ONLY,subnet=subnet-jakarta-1 \
+    --maintenance-policy=TERMINATE \
+    --provisioning-model=STANDARD \
+    --service-account=232168920061-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --accelerator=count=1,type=nvidia-tesla-t4 \
+    --create-disk=auto-delete=yes,boot=yes,device-name=gpu-jakarta-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240126,mode=rw,size=80,type=projects/law-2006463162/zones/asia-southeast2-b/diskTypes/pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+
+# Controller
+gcloud compute instances create controller-jakarta-1 \
+    --project=law-2006463162 \
+    --zone=asia-southeast2-b \
+    --machine-type=e2-custom-4-8192 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --network-interface=aliases=/26,network-tier=STANDARD,stack-type=IPV4_ONLY,subnet=subnet-jakarta-1 \
+    --provisioning-model=STANDARD \
+    --service-account=232168920061-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=gpu-jakarta-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240126,mode=rw,size=80,type=projects/law-2006463162/zones/asia-southeast2-b/diskTypes/pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
