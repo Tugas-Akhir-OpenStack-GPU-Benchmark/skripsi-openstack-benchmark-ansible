ea8adab98d7d308b5d77a32e594b3a2df1f8fd19
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-07 16:56:52 +0700

    feat: checkpoint implement multinode devstack up till the end. not tested yet

--- devstack_run.sh
+++ devstack_run.sh
@@ -6,5 +6,5 @@ touch "temp/installed/community.general"
 
 export ANSIBLE_EXTRAVARS="ansible_user=stack ansible_ssh_user=immanuel01 ansible_ssh_private_key_file=~/.ssh/gcp"
 
-#sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS"
-sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS" --start-at-task="check current username"
+sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS"
+#sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS" --start-at-task="check current username"
