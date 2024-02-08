0c755db7bb7a1f4fc8c92398eb7552edd75d7de5
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-09 00:09:14 +0700

    devstack multinode tested and seem to be fine. Note: I think devstack's stack.sh is not idempotent up to this point

--- auto_turn_off_run.sh
+++ auto_turn_off_run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 # need trailing comma
-sudo ansible-playbook ./tasks/auto_turnoff/auto_turnoff.yaml -i ./tasks/auto_turnoff/auto_turnoff_inventory.txt -e "ansible_host=$1"
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit
+fi
+
+i=0
+for var in "$@"
+do
+    echo "$i setting auto shutdown for $var"
+    ((i+=1))
+    sudo ansible-playbook ./tasks/auto_turnoff/auto_turnoff.yaml -i ./tasks/auto_turnoff/auto_turnoff_inventory.txt -e "ansible_host=$var"
+    echo "$i setting auto shutdown for $var"
+done
+echo "done"
--- tasks/devstack/data/controller-local.sh
+++ tasks/devstack/data/controller-local.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-for i in `seq 2 10`; do /opt/stack/nova/bin/nova-manage fixed reserve 10.4.128.$i; done
+
+# Last error:
+# nova-manage: error: argument category: invalid choice: 'fixed' (choose from 'version', 'bash-completion', 'api_db', 'cell_v2', 'db', 'placement', 'libvirt', 'volume_attachment', 'image_property', 'limits')
+# So we comment-out this thing
+
+# for i in `seq 2 10`; do /usr/local/bin/nova-manage fixed reserve 10.4.128.$i; done
