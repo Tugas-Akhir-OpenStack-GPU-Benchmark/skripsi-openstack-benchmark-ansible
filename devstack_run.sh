b156f5346f6a78ffc1a1661ce9bbebd6d476c84c
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-11 13:10:38 +0700

    feat: simplify devstack_run.sh to allow us running it without modifying inventory.txt everytime new public IP addresses generated by GCP

--- devstack_run.sh
+++ devstack_run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-mkdir -p "temp/installed"
-test ! -e "temp/installed/community.general" && ansible-galaxy collection install community.general  # v8.3.0
-touch "temp/installed/community.general"
+if [ $# -lt 2 ]
+then
+    echo "Need 2 arguments: CONTROLLER_SSH_IP COMPUTE_SSH_IP"
+    exit
+fi
+
+
+#mkdir -p "temp/installed"
+#test ! -e "temp/installed/community.general" && ansible-galaxy collection install community.general  # v8.3.0
+#touch "temp/installed/community.general"
 
 export ANSIBLE_EXTRAVARS="ansible_user=stack ansible_ssh_user=immanuel01 ansible_ssh_private_key_file=~/.ssh/gcp"
+export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS CONTROLLER_SSH_IP=$1 COMPUTE_SSH_IP=$2"
 
-sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS"
+sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.yml -e "$ANSIBLE_EXTRAVARS"
 #sudo ansible-playbook ./tasks/devstack/main.yaml -i ./tasks/devstack/inventory.txt -e "$ANSIBLE_EXTRAVARS" --start-at-task="check current username"
