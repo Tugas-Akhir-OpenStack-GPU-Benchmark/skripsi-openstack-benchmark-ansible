a051d18c4271d20fcc9cc98d9447ab2092417e4d
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-12 23:27:16 +0700

    fix: modify GCP-instance to have multiple IP, to be used as floating-ip of the OpenStack

--- gcp-spec/1-nic/gcp-belgium-2-nic.sh
+++ gcp-spec/1-nic/gcp-belgium-2-nic.sh
@@ -3,12 +3,14 @@ gcloud compute instances create gpu-belgium-1 \
     --project=law-2006463162 \
     --zone=europe-west1-d \
     --machine-type=custom-4-8192 \
-    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --network-interface=aliases=10.132.2.0/26,network-tier=STANDARD,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
     --maintenance-policy=TERMINATE \
     --provisioning-model=STANDARD \
     --service-account=232168920061-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
     --accelerator=count=1,type=nvidia-tesla-t4 \
+    --tags=http-server,https-server,lb-health-check \
     --create-disk=auto-delete=yes,boot=yes,device-name=gpu-belgium-1-gpu,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240126,mode=rw,size=80,type=projects/law-2006463162/zones/europe-west1-d/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
@@ -17,15 +19,18 @@ gcloud compute instances create gpu-belgium-1 \
     --reservation-affinity=any
 
 
+
 # Controller
-gcloud compute instances create controller-belgium-1 \
+gcloud compute instances create controller-belgium-2 \
     --project=law-2006463162 \
     --zone=europe-west1-d \
     --machine-type=e2-custom-4-8192 \
-    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --network-interface=aliases=10.132.1.0/26,network-tier=STANDARD,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
     --provisioning-model=STANDARD \
     --service-account=232168920061-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --tags=http-server,https-server,lb-health-check \
     --create-disk=auto-delete=yes,boot=yes,device-name=gpu-belgium-1-controller,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240126,mode=rw,size=80,type=projects/law-2006463162/zones/europe-west1-d/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
