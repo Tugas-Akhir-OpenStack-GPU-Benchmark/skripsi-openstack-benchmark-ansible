e2d80df443627b278c4616075ff171f1887ec298
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-02-19 20:16:11 +0700

    feat: basic Maas install

---
+++ juju_run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+if [ $# -lt 2 ]
+then
+    echo "Need at least 2 arguments: CONTROLLER_SSH_IP COMPUTE_SSH_IP"
+    exit
+fi
+
+ssh_target_compute_node="${!#}"
+
+second_last=$(($#-2))
+ssh_target_controller_node="${@:(-2):1}"
+
+
+# Combine the remaining arguments into a string with single space separator
+argument_length=$(($#-2))
+ssh_jumphosts=${@:1:$argument_length}
+
+echo "ssh_target_controller_node: $ssh_target_controller_node"
+echo "ssh_target_compute_node: $ssh_target_compute_node"
+echo "num of jumphosts: $argument_length"
+
+
+# if more than 1 arguments specified
+if (( $argument_length > 0 )); then
+    ssh_jumphosts="-A -J $ssh_jumphosts"
+    echo "jumphost detected"
+fi
+
+echo "ssh_jumphosts: $ssh_jumphosts"
+
+
+if [[ $ssh_target_controller_node != *@* ]]; then
+    ssh_target_controller_node_ip="$ssh_target_controller_node"
+    ssh_target_controller_node_username="immanuel01"
+else
+    IFS='@' read -r ssh_target_controller_node_username ssh_target_controller_node_ip <<< "$ssh_target_controller_node";
+fi
+if [[ $ssh_target_compute_node != *@* ]]; then
+    ssh_target_compute_node_ip="$ssh_target_compute_node"
+    ssh_target_compute_node_username="immanuel01"
+else
+    IFS='@' read -r ssh_target_compute_node_username ssh_target_compute_node_ip <<< "$ssh_target_compute_node";
+fi
+
+
+# Assumption appropriate ssh-key already exists (on the computer which run this ansible script)
+# and already set as default (can be configured inside the ~/.ssh/config)
+
+export ANSIBLE_EXTRAVARS=""
+#export ANSIBLE_EXTRAVARS="ansible_user=stack"
+export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS ansible_ssh_common_args=\"$ssh_jumphosts\""
+export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS CONTROLLER_SSH_USER=$ssh_target_controller_node_username"
+export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS COMPUTE_SSH_USER=$ssh_target_compute_node_username"
+export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS CONTROLLER_SSH_IP=$ssh_target_controller_node_ip COMPUTE_SSH_IP=$ssh_target_compute_node_ip"
+
+if [ -n "$SUDO_PASS" ]; then
+  ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS ansible_sudo_pass=\"$SUDO_PASS\""
+  echo "detected SUDO_PASS. Will use it for sudo authentication"
+else
+  echo "var SUDO_PASS is not set. Will assume target hosts no need password to run sudo commands..."
+fi
+
+
+export ANSIBLE_CACHE_PLUGIN=jsonfile
+
+sudo ansible-playbook ./tasks/juju/main.yaml -i ./tasks/juju/inventory.yml -e "$ANSIBLE_EXTRAVARS"
---
+++ tasks/juju/data/get_network_information.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import json
+import sys
+from pprint import pprint
+import netifaces
+import psutil
+
+
+def main(assert_nic_exists: list[str]):
+    nic_information = psutil.net_if_addrs()
+
+    nic_information = {
+        key: {
+            "ipaddr": value[0].address,
+            "netmask": value[0].netmask,
+            "broadcast": value[0].broadcast,
+        }
+        for (key, value) in nic_information.items()
+        if startswith_any(key, "ens", "eth", "vlan")
+    }
+    list_of_nic = set(nic_information.keys())
+    # assert len(list_of_nic) >= 2, "At least 2 NIC should be installed"
+
+    for nic in assert_nic_exists:
+        assert nic in nic_information
+    default_gateway = get_default_gateway_linux()
+    print(json.dumps({
+        "gateway": default_gateway,
+        "nic": nic_information,
+        "interface_1": default_gateway['nic'],
+        "interface_2": unpack_set(list_of_nic - {default_gateway['nic']}),
+    }))
+
+
+def startswith_any(string: str, *prefixes):
+    for prefix in prefixes:
+        if string.startswith(prefix):
+            return True
+    return False
+
+
+def get_default_gateway_linux():
+    default_gateways = netifaces.gateways()['default'].values()
+    gateway_ip_address, nic = unpack_set(default_gateways)
+    return {
+        "ipaddr": gateway_ip_address,
+        "nic": nic
+    }
+
+
+def unpack_set(set_: set):
+    if len(set_) == 0:
+        return None
+    sorted_set = sorted(set_)
+    return sorted_set[0]
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
