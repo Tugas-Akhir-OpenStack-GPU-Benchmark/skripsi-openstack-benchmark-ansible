47b1f395b7bc3914672a83c6608a5b4c465bcbb6
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-03-14 23:41:07 +0700

    feat: add option to disable GPU

--- tasks/devstack/data/controller-startup-script.sh
+++ tasks/devstack/data/controller-startup-script.sh
@@ -3,4 +3,4 @@
 sudo ip link set  br-ex up
 sudo ip route add {{ devstack_floating_ip_range }} dev br-ex
 sudo iptables -t nat -A POSTROUTING -o {{ interface_1 }} -j MASQUERADE
-sudo ip li set mtu 1200 dev br-ex
+#sudo ip li set mtu 1200 dev br-ex
