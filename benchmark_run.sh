de63cc1de4fb42023abbf4f60542c5ccee75b5fa
Immanuel <142286118+t-immanuel@users.noreply.github.com>
2024-04-21 03:07:29 +0700

    add SUDO_PASS to benchmark_run.sh

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -27,4 +27,12 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_EXTRAVARS="ansible_host=$1 ansible_user=$ANSIBLE_USER ansible_ssh_private_key_file=~/.ssh/gcp"
 export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS ansible_ssh_common_args=\"$ansible_ssh_common_args\""
 export ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS ansible_ssh_extra_args=\"$ansible_ssh_extra_args\""
+
+if [ -n "$SUDO_PASS" ]; then
+  ANSIBLE_EXTRAVARS="$ANSIBLE_EXTRAVARS ansible_sudo_pass=\"$SUDO_PASS\""
+  echo "detected SUDO_PASS. Will use it for sudo authentication"
+else
+  echo "var SUDO_PASS is not set. Will assume target hosts don't need password to run sudo commands..."
+fi
+
 sudo ansible-playbook ./tasks/benchmark/main.yaml -i ./tasks/benchmark/inventory.txt -e "$ANSIBLE_EXTRAVARS"
