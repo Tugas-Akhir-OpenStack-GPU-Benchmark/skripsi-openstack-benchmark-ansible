c3ad68a3f6b7bcb3db02dabc94dfef4b4235ff93
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2024-05-07 21:06:43 +0700

    feat: modify benchmark run with async support

--- benchmark_run.sh_async.sh
+++ benchmark_run.sh_async.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo env SUDO_PASS="$SUDO_PASS" nohup ./benchmark_run.sh "$@" 2>&1 > "nohup_log $(date '+%F %H:%M:%S').txt" &
+if [ -z "$ANSIBLE_CONNECTION_FOR_TARGET_HOST" ]; then
+  echo "ANSIBLE_CONNECTION_FOR_TARGET_HOST is not set... WIll use ssh as the connection method"
+  ANSIBLE_CONNECTION_FOR_TARGET_HOST="ssh"
+else
+  echo "detected ANSIBLE_CONNECTION_FOR_TARGET_HOST=$ANSIBLE_CONNECTION_FOR_TARGET_HOST"
+fi
+
+sudo env ANSIBLE_CONNECTION_FOR_TARGET_HOST=\"$ANSIBLE_CONNECTION_FOR_TARGET_HOST\"" SUDO_PASS="$SUDO_PASS" nohup ./benchmark_run.sh "$@" 2>&1 > "nohup_log $(date '+%F %H:%M:%S').txt" &
